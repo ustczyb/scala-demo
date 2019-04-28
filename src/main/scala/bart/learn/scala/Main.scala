a1f019b60025b9dc508616e165b2f4e5bfc479db
张育博 <zhangyubo02@meituan.com>
2019-04-28 20:18:55 +0800

    初始化学习scala的项目

---
+++ src/main/scala/bart/learn/scala/Main.scala
@@ -0,0 +1,12 @@
+package bart.learn.scala
+
+/**
+  * @author zhangyubo02
+  *         create time : 2019/4/28. 
+  */
+object Main {
+
+    def main(args: Array[String]): Unit = {
+        println("hello world")
+    }
+}
